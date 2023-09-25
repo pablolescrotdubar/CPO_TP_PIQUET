0e744b830563ce279fbad68489d426909f036d64
nicolas piquet <nicolas.piquet@epfedu.fr>
2023-09-25 18:50:45 +0200

    Exo terminé

--- Calculator/src/calculator/Calculator.java
+++ Calculator/src/calculator/Calculator.java
@@ -1,5 +1,5 @@
 /*
- Nicolas Piquet
+Nicolas Piquet
 TP0
 25 septembre 2023 
  */
@@ -18,17 +18,24 @@ public class Calculator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
         int operateur;
         System.out.println("Please enter the operator :"+"\n1)add"+"\n2)substract"+"\n3)multiply"+"\n4)divide"+"\n5)modulo");
         Scanner sc=new Scanner(System.in);
-        System.out.println("\n Entrez le numero de votre operation :");
-        operateur=sc.nextInt();//On demande a sc de donner le prochain entier
         
-        System.out.println("Entrez une première valeur :");
+        do {
+            System.out.println("\n Entrez le numero de votre operation :");
+            operateur=sc.nextInt();//On demande a sc de donner le prochain entier
+            if (operateur < 1 || operateur > 5) {
+                System.out.println("erreur");
+            }
+        } while (operateur < 1 || operateur > 5);
+        
+        System.out.println("Entrez une premiere valeur :");
         
         double operande1 = sc.nextInt();
         
-        System.out.println("Entrez une deuxième valeur :");
+        System.out.println("Entrez une deuxieme valeur :");
         
         double operande2 = sc.nextInt();
         
