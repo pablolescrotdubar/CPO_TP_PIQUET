d79da24a6c5d762c68f8c98e932c3221ce99304a
nicolas piquet <nicolas.piquet@epfedu.fr>
2023-09-25 15:28:35 +0200

    Exo terminé

---
+++ Exo1/src/main/java/com/mycompany/exo1/Exo1.java
@@ -0,0 +1,25 @@
+/*
+Nicolas Piquet
+TP0
+18 septembre 2023 
+ */
+
+package com.mycompany.exo1;
+
+import java.util.Scanner;
+
+/**
+ Piquet Nicolas_TDB_AffichageMessage_18/09/2023
+ */
+public class Exo1 {
+
+    public static void main(String[] args) {
+        System.out.println("Bonjour");
+        System.out.println("Au revoir");
+        String prenom;
+        Scanner sc;
+        sc=new Scanner(System.in);
+        System.out.println("Bonjour, quel est votre prenom ?");
+        prenom=sc.nextLine();
+    }
+}
