00ab4947dd6074583261725e47085cb049b856ed
nicolas piquet <nicolas.piquet@epfedu.fr>
2023-10-03 15:08:07 +0200

    Guessmynumber terminé

--- TP1_guessMyNumber_PIQUET/src/tp1_guessmynumber_piquet/TP1_guessMyNumber_PIQUET.java
+++ TP1_guessMyNumber_PIQUET/src/tp1_guessmynumber_piquet/TP1_guessMyNumber_PIQUET.java
@@ -19,26 +19,81 @@ public class TP1_guessMyNumber_PIQUET {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        int numniveau;
         Scanner sc=new Scanner(System.in);
+        System.out.println("Veuillez choisir un niveau de difficulte :");
+        System.out.println("1) Facile");
+        System.out.println("2) Moyen");
+        System.out.println("3) Difficile");
+        numniveau = sc.nextInt();
         Random generateurAleat = new Random();
         int valalea;
         int valentree = -1;
-        valalea = generateurAleat.nextInt(100);
-        while (valentree != valalea) {
-            System.out.println("Saisissez une valeur entiere entre 0 et 100 :");
-            valentree = sc.nextInt();
-            if (valentree < 0 || valentree > 100) {
-                System.out.println("Saisissez une valeur entiere entre 0 et 100 :");
-                valentree = sc.nextInt();
-            }
-            if (valentree < valalea) {
-                System.out.println("Trop petit");
-            } else if (valentree > valalea) {
-                System.out.println("Trop grand");
-            } else if (valentree == valalea) {
-                System.out.println("Gagne");
-            }
+        int compteur = 0;
+        while (numniveau<1 || numniveau>3) {
+            System.out.println("Veuillez reessayer :");
+            numniveau = sc.nextInt();
         }
-        
+        switch (numniveau) {
+            case 1 :
+                valalea = generateurAleat.nextInt(50);
+                while (valentree != valalea) {
+                    System.out.println("Saisissez une valeur entiere entre 0 et 50 :");
+                    valentree = sc.nextInt();
+                    while (valentree < 0 || valentree > 50) {
+                        System.out.println("Saisissez une valeur entiere entre 0 et 50 :");
+                        valentree = sc.nextInt();
+                    }
+                    if (valentree < valalea) {
+                        System.out.println("Trop petit");
+                    } else if (valentree > valalea) {
+                        System.out.println("Trop grand");
+                    } else if (valentree == valalea) {
+                        System.out.println("Gagne");
+                    }
+                    compteur += 1;
+                }
+                System.out.println("Vous avez gagné en "+compteur+" coups");
+            
+            case 2 :
+                valalea = generateurAleat.nextInt(100);
+                while (valentree != valalea) {
+                    System.out.println("Saisissez une valeur entiere entre 0 et 100 :");
+                    valentree = sc.nextInt();
+                    while (valentree < 0 || valentree > 100) {
+                        System.out.println("Saisissez une valeur entiere entre 0 et 100 :");
+                        valentree = sc.nextInt();
+                    }
+                    if (valentree < valalea) {
+                        System.out.println("Trop petit");
+                    } else if (valentree > valalea) {
+                        System.out.println("Trop grand");
+                    } else if (valentree == valalea) {
+                        System.out.println("Gagne");
+                    }
+                    compteur += 1;
+                }
+                System.out.println("Vous avez gagné en "+compteur+" coups");
+                
+            case 3 :
+                valalea = generateurAleat.nextInt(200);
+                while (valentree != valalea) {
+                    System.out.println("Saisissez une valeur entiere entre 0 et 200 :");
+                    valentree = sc.nextInt();
+                    while (valentree < 0 || valentree > 200) {
+                        System.out.println("Saisissez une valeur entiere entre 0 et 200 :");
+                        valentree = sc.nextInt();
+                    }
+                    if (valentree < valalea) {
+                        System.out.println("Trop petit");
+                    } else if (valentree > valalea) {
+                        System.out.println("Trop grand");
+                    } else if (valentree == valalea) {
+                        System.out.println("Gagne");
+                    }
+                    compteur += 1;
+                }
+                System.out.println("Vous avez gagné en "+compteur+" coups");
+        }
     }
-}    
+}
