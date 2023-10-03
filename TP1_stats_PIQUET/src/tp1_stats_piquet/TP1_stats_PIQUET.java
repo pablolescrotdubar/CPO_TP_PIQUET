ff2931276ed5fae8d726107dfbf433ba525a8363
nicolas piquet <nicolas.piquet@epfedu.fr>
2023-10-03 15:30:13 +0200

    stats en cours

---
+++ TP1_stats_PIQUET/src/tp1_stats_piquet/TP1_stats_PIQUET.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp1_stats_piquet;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ Nicolas Piquet
+* TP1
+* Guessmynumber
+* 03/10/23
+ */
+public class TP1_stats_PIQUET {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int tab [] = new int[6];
+        for (int i = 0; i <= 5; i++) {
+            tab[i] = 0;
+            System.out.println(tab[i]);
+        }
+        Scanner sc=new Scanner(System.in);
+    }
+}
