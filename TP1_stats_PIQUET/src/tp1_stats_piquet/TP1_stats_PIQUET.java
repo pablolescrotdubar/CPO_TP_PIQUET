2c0f6c498945bc5a6a067700be0fdc3ca09f9892
nicolas piquet <nicolas.piquet@epfedu.fr>
2023-10-04 12:06:12 +0200

    stats terminé

--- TP1_stats_PIQUET/src/tp1_stats_piquet/TP1_stats_PIQUET.java
+++ TP1_stats_PIQUET/src/tp1_stats_piquet/TP1_stats_PIQUET.java
@@ -22,8 +22,25 @@ public class TP1_stats_PIQUET {
         int tab [] = new int[6];
         for (int i = 0; i <= 5; i++) {
             tab[i] = 0;
-            System.out.println(tab[i]);
+            /*System.out.println(tab[i]);
+            */
         }
         Scanner sc=new Scanner(System.in);
+        int nb;
+        System.out.println("Veuillez saisir un entier strictement positif :");
+        nb = sc.nextInt();
+        while (nb<1) {
+            System.out.println("Veuillez saisir un entier strictement positif :");
+            nb = sc.nextInt();
+        }
+        Random generateurAleat = new Random();
+        int valalea;
+        for (int i=0; i<nb; i++) {
+            valalea = generateurAleat.nextInt(5);
+            tab[valalea]+=1;
+        }
+        for (int j = 0; j<6; j++) {
+            System.out.println((j+1)+" : "+((tab[j]/(nb*1.0))*100)+" %");
+        }
     }
 }
