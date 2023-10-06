418ff2cbce8fc305ab2305700cf2271e713a68d1
nicolas piquet <nicolas.piquet@epfedu.fr>
2023-10-06 17:46:49 +0200

    TP2 bieres terminé

---  TP2_Bieres_PIQUET/src/tp2_bieres_piquet/TP2_Bieres_PIQUET.java
+++  TP2_Bieres_PIQUET/src/tp2_bieres_piquet/TP2_Bieres_PIQUET.java
@@ -11,11 +11,30 @@ package tp2_bieres_piquet;
  */
 public class TP2_Bieres_PIQUET {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls", 7.0, "Dubuisson") ;
+        uneBiere.ouverte = false;
+        //uneBiere.lireEtiquette();
+        
+        
+        
+        BouteilleBiere deuxBieres = new BouteilleBiere("Leffe", 6.6, "Abbaye de Leffe");
+        deuxBieres.ouverte = false;
+        //deuxBieres.lireEtiquette();
+        
+        BouteilleBiere MontBlanc1 = new BouteilleBiere("Rousse", 5.0, "Cham") ;
+        MontBlanc1.ouverte = false;
+        //MontBlanc1.lireEtiquette();
+        
+        BouteilleBiere MontBlanc2 = new BouteilleBiere("Blanche", 6.0, "Cham") ;
+        MontBlanc2.ouverte = false;
+        //MontBlanc2.lireEtiquette();
+        
+        BouteilleBiere MontBlanc3 = new BouteilleBiere("Blonde", 7.0, "Cham") ;
+        MontBlanc3.ouverte = true;
+        //MontBlanc3.lireEtiquette();
+        MontBlanc3.Decapsuler();
+        
+        System.out.println(MontBlanc3);
     }
-    
 }
