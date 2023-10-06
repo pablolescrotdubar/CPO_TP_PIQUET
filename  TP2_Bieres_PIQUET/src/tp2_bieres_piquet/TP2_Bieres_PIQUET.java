492404c1e3ce4caf8313dee3ff3e4238ca609415
nicolas piquet <nicolas.piquet@epfedu.fr>
2023-10-06 15:53:27 +0200

    TP2 bieres en cours

---
+++  TP2_Bieres_PIQUET/src/tp2_bieres_piquet/TP2_Bieres_PIQUET.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_bieres_piquet;
+
+/**
+* Nicolas Piquet
+* TP2 Bieres
+* 06/10/2023
+ */
+public class TP2_Bieres_PIQUET {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
