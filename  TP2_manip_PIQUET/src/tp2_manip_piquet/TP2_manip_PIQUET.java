25e5b96d80d0bdb87ea80f932f6ac32a75da1120
nicolas piquet <nicolas.piquet@epfedu.fr>
2023-10-13 11:15:22 +0200

    TP2 manips en cours

---
+++  TP2_manip_PIQUET/src/tp2_manip_piquet/TP2_manip_PIQUET.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_manip_piquet;
+
+/**
+ * Nicolas Piquet
+ * 13/10/2023
+ * TP2 Manips
+ */
+public class TP2_manip_PIQUET {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Tartiflette assiette1 = new Tartiflette(500);
+        Tartiflette assiette2 = new Tartiflette(600);
+        Tartiflette assiette3 = assiette2; 
+    }
+    
+}
