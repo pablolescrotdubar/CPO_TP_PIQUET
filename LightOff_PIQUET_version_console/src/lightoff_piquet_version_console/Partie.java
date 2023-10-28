0db69c92f6da71b288f81accb2e446265b1dc0c6
nicolas piquet <nicolas.piquet@epfedu.fr>
2023-10-28 16:34:23 +0200

    Version de base terminée

--- LightOff_PIQUET_version_console/src/lightoff_piquet_version_console/Partie.java
+++ LightOff_PIQUET_version_console/src/lightoff_piquet_version_console/Partie.java
@@ -16,15 +16,27 @@ public class Partie {
     int nbCoups;
     int n;
 
+    /**
+     * Crée une partie à l'aide d'une grille de jeu de taille n*n.
+     * Initialise le nombre de coups à 0
+     * @param n
+     */
     public Partie(int n) {
         this.grille = new GrilledeJeu(n, n);
         this.nbCoups = 0;
     }
     
+    /**
+     * Initialise la grille de jeu en mélangeant aléatoirement la grille d'un nombre de coups donné
+     */
     public void initialiserPartie() {
         this.grille.melangerMatriceAleatoirement(5);
     }
     
+    /**
+     * Affiche l'état initial de la grille et effectue les coups demandés par l'utilisateur jusqu'à ce que
+     * toutes les cellules soient éteintes.
+     */
     public void lancerPartie() {
         Scanner sc = new Scanner(System.in);
         this.initialiserPartie();
@@ -32,9 +44,11 @@ public class Partie {
         while (this.grille.cellulesToutesEteintes() == false) {
             System.out.println("\n"+"Veuillez choisir un coup : L pour une lignes a activer, C pour une colonne et D pour une diagonale :");
             char cas = sc.next().charAt(0);
+            sc.nextLine();
             while (cas != 'L' && cas != 'C' && cas != 'D') {
                 System.out.println("\n"+"Veuillez reessayer :");
                 cas = sc.next().charAt(0);
+                sc.nextLine();
             }
             switch (cas) {
                 case 'L' :
@@ -46,8 +60,8 @@ public class Partie {
                     }
                     this.grille.activerLigneDeCellules(numligne);
                     nbCoups++;
+                    System.out.println("\n"+this.grille);
                     System.out.println("\n"+"Nombre de coups realises : "+nbCoups);
-                    System.out.println(this.grille);
                     break;
                 case 'C' :
                     System.out.println("\n"+"Entrez le numéro de la colonne a activer :");
@@ -58,13 +72,13 @@ public class Partie {
                     }
                     this.grille.activerColonneDeCellules(numcolonne);
                     nbCoups++;
+                    System.out.println("\n"+this.grille);
                     System.out.println("\n"+"Nombre de coups realises : "+nbCoups);
-                    System.out.println(this.grille);
                     break;
                 case 'D' :
                     System.out.println("\n"+"Entrez DD pour activer la diagonale decendante et DM pour la diagonale montante :");
                     String casdiag = sc.nextLine();
-                    while (casdiag != "DD" || casdiag != "DM") {
+                    while (!casdiag.equals("DD") && !casdiag.equals("DM")) {
                         System.out.println("\n"+"Veuillez reessayer :");
                         casdiag = sc.nextLine();
                     }
@@ -72,14 +86,14 @@ public class Partie {
                         case "DD" :
                             this.grille.activerDiagonaleDescendante();
                             nbCoups++;
+                            System.out.println("\n"+this.grille);
                             System.out.println("\n"+"Nombre de coups realises : "+nbCoups);
-                            System.out.println(this.grille);
                             break;
                         case "DM" :
                             this.grille.activerDiagonaleMontante();
                             nbCoups++;
+                            System.out.println("\n"+this.grille);
                             System.out.println("\n"+"Nombre de coups realises : "+nbCoups);
-                            System.out.println(this.grille);
                             break;
                     }
         
